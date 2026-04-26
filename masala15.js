b7a6ef7599dd85e9ff214e8ceb43b9b519ae716f
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-26 20:15:53 +0500

    Uncomment code for input and calculations

---
+++ masala15.js
@@ -0,0 +1,17 @@
+// 15-misol tarmowlanuvchi 5
+
+let a = Number(prompt('a:'))
+let b = Number(prompt('b:'))
+let c = Number(prompt('c:'))
+
+let mod1 = Math.fabs(a)
+let mod2 = Math.fabs(b)
+let mod3 = Math.fabs(c)
+if (a>=b >= c) {
+    alert(a * 2)
+    alert(b * 2)
+    alert(c * 2)
+}
+else{
+    alert(a,b,c)
+}
