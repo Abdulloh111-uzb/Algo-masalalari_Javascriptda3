258c608ee43c599049ed26e8653f2a0eca10d2de
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-27 11:12:30 +0500

    Create masala20.js

---
+++ masala20.js
@@ -0,0 +1,9 @@
+// 20-misol tarmoqlanuvchi 9
+
+let x = Number(prompt('x:'))
+let y = Number(prompt('y:'))
+
+let yarim = (x + y) / 2
+let ikkilangan = 2 * x * y
+console.log(yarim)
+console.log(ikkilangan)
