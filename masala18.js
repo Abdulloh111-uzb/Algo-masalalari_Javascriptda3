c5774284c3f46310d5b7490aec1380f8370de0a5
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-27 11:02:30 +0500

    Create masala18.js

---
+++ masala18.js
@@ -0,0 +1,13 @@
+// 18-misol tarmoqlanuvchi 8
+let x = Number(prompt('x:'))
+let y = Number(prompt('y:'))
+let z = Number(prompt('z:'))
+if (1 < x < 3) {
+    alert(x)
+}
+else if (1 < y < 3) {
+    alert(y)
+}
+else if (1 < z < 3) {
+    alert(z)
+}
