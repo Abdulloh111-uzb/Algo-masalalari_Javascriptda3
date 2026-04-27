474f5dce2357de4002b595ef2b8278fa25472a93
Tohirov Abdulloh <tohirovsarvar219@gmail.com>
2026-04-27 11:06:39 +0500

    Add script to calculate square of positive numbers

---
+++ 19-masala.js
@@ -0,0 +1,15 @@
+// 19-misol tarmoqlanuvchi 10
+
+let x = Number(prompt('x:'))
+let y = Number(prompt('y:'))
+let z = Number(prompt('z:'))
+
+if (x > 0) {
+    alert(Math.pow(x,2))
+}
+else if (y > 0) {
+    alert(Math.pow(y,2))
+}
+else if (z > 0) {
+    alert(Math.pow(z,2))
+}
